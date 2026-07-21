51951be4f0f890b099abec406d427a243c4b91b4
James-Ntwali <j.ntwali@alustudent.com>
2026-07-21 12:56:29 +0200

    james

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,57 +1,17 @@
 #!/usr/bin/python3
-"""
-This module contains a function to query the Reddit API and print the titles
-of the first 10 hot posts for a given subreddit.
-"""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    for a given subreddit.
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
-    Args:
-        subreddit (str): The name of the subreddit to query.
-
-    Returns:
-        None: Prints the titles or None if invalid subreddit.
-    """
-    if subreddit is None or not isinstance(subreddit, str):
-        print(None)
-        return
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                      'AppleWebKit/537.36'
-    }
-
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    params = {'limit': 10}
-
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
     try:
-        response = requests.get(
-            url,
-            headers=headers,
-            params=params,
-            allow_redirects=False
-        )
-
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-
-            if not posts:
-                print(None)
-                return
-
-            for post in posts:
-                title = post.get('data', {}).get('title')
-                if title:
-                    print(title)
-        else:
-            print(None)
-
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
     except Exception:
         print(None)
