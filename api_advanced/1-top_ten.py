940183b7ae6f1be2f48db411336cdb654bf43d25
Ntwali-creator <j.ntwali@alustudent.com>
2026-07-21 04:18:03 -0700

    Fix: Standard ALU solution for top_ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,30 +1,43 @@
 #!/usr/bin/python3
-"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+"""
+Module for querying the Reddit API and printing the top 10 hot posts.
+"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the first 10 hot posts of a subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    """
+    Prints the titles of the first 10 hot posts for a given subreddit.
+    If invalid subreddit, prints None.
+    """
+    if subreddit is None or type(subreddit) is not str:
+        print(None)
+        return
 
-    headers = {
-        "User-Agent": "ALU-api-project/1.0"
-    }
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "MyBot/1.0"}
+    params = {"limit": 10}
 
     try:
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False)
+        response = requests.get(
+            url,
+            headers=headers,
+            params=params,
+            allow_redirects=False
+        )
 
-        data = response.json()
-        posts = data.get("data", {}).get("children")
+        if response.status_code == 200:
+            data = response.json()
+            posts = data.get("data", {}).get("children", [])
 
-        if posts is None:
+            if posts:
+                for post in posts:
+                    print(post.get("data", {}).get("title"))
+            else:
+                print(None)
+        else:
             print(None)
-            return
-
-        for post in posts[:10]:
-            print(post.get("data", {}).get("title"))
 
     except Exception:
         print(None)
